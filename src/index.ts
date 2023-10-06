af8e201227b675bcf0930e14900f915cd2f536fd
Valentina <korolenkovalya0704@gmail.com>
2023-10-06 11:16:21 +0300

    add

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,20 @@ type DeepReadonly<T> = {
   readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K];
 };
 
+/*const user = {
+  name: "Valya",
+  age: 32,
+  address: {
+    street: "Studentska St",
+    city: "Kharkiv",
+  },
+};
+const readonlyUser: DeepReadonly<typeof user> = user;
+
+readonlyUser.name = "Ann";
+readonlyUser.address.street = "Example St";*/
+
+
 // Вам потрібно створити тип DeepRequireReadonly який буде робити доступними тільки для читання
 //  навіть властивості вкладених обʼєктів та ще й робити їх обовʼязковими.
 
@@ -10,11 +24,43 @@ type DeepRequireReadonly<T> = {
   readonly [K in keyof T]-?: T[K] extends object ? DeepRequireReadonly<T[K]> : T[K];
 };
 
+
+
+/*interface Person {
+  name: string;
+  age?: number;
+  address: {
+    street: string;
+    city: string;
+}
+}
+
+const readonlyRequiredPerson: DeepRequireReadonly<Person> = {
+  name: "Valya",
+  age: 32,
+  address: {
+    street: "Studentska St",
+    city: "Kharkiv",
+  },
+};*/
+
+
+
 // Вам потрібно сворити тип UpperCaseKeys, який буде приводити всі ключи до верхнього регістру.
 
 type UpperCaseKeys<T> = {
   [K in keyof T as Uppercase<string & K>]: T[K];
 };
+
+/*const myObject = {
+  age: 32,
+  name: "Valya",
+  city:"Kharkiv"
+};
+
+type UppercaseKeysObject = UpperCaseKeys<typeof myObject>;*/
+
+
 
 // І саме цікаве.
 //  Створіть тип ObjectToPropertyDescriptor, який перетворює звичайний обʼєкт на обʼєкт де кожне value є дескриптором.
@@ -30,10 +76,11 @@ const myObject = {
   
 };
 
-// const propertyDescriptorObject: ObjectToPropertyDescriptor<typeof myObject> = {
-//   age: { value: 32, writable: true, enumerable: true, configurable: true },
-//   name: { value: "Valya", writable: true, enumerable: true, configurable: true },
-// };
+/*const propertyDescriptorObject: ObjectToPropertyDescriptor<typeof myObject> = {
+  age: { value: 32, writable: true, enumerable: true, configurable: true },
+  name: { value: "Valya", writable: true, enumerable: true, configurable: true },
+};*/
+
 const propertyDescriptors: ObjectToPropertyDescriptor<typeof myObject> = {
   name: Object.getOwnPropertyDescriptor(myObject, "name")!,
   age: Object.getOwnPropertyDescriptor(myObject, "age")!,
