d516ddb4a70963bfd62ed350b791d560dcf35173
Valentina <korolenkovalya0704@gmail.com>
2023-10-06 10:40:00 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+// Вам потрібно створити тип DeepReadonly який буде робити доступними тільки для читання навіть властивості вкладених обʼєктів.
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K];
+};
+
+// Вам потрібно створити тип DeepRequireReadonly який буде робити доступними тільки для читання
+//  навіть властивості вкладених обʼєктів та ще й робити їх обовʼязковими.
+
+type DeepRequireReadonly<T> = {
+  readonly [K in keyof T]-?: T[K] extends object ? DeepRequireReadonly<T[K]> : T[K];
+};
+
+// Вам потрібно сворити тип UpperCaseKeys, який буде приводити всі ключи до верхнього регістру.
+
+type UpperCaseKeys<T> = {
+  [K in keyof T as Uppercase<string & K>]: T[K];
+};
+
+// І саме цікаве.
+//  Створіть тип ObjectToPropertyDescriptor, який перетворює звичайний обʼєкт на обʼєкт де кожне value є дескриптором.
+
+type ObjectToPropertyDescriptor<T> = {
+  [K in keyof T]: PropertyDescriptor;
+};
+
+
+const myObject = {
+  age: 32,
+  name: "Valya",
+  
+};
+
+// const propertyDescriptorObject: ObjectToPropertyDescriptor<typeof myObject> = {
+//   age: { value: 32, writable: true, enumerable: true, configurable: true },
+//   name: { value: "Valya", writable: true, enumerable: true, configurable: true },
+// };
+const propertyDescriptors: ObjectToPropertyDescriptor<typeof myObject> = {
+  name: Object.getOwnPropertyDescriptor(myObject, "name")!,
+  age: Object.getOwnPropertyDescriptor(myObject, "age")!,
+};
+
